3c16946424b1b9d75f7815d8a5543e8463c0be49
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-03-05 06:57:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -86,7 +86,7 @@ def play(board)
   end
 
   if won?(board)
-    puts "Congratulations #{current_player(board)}!"
+    puts "Congratulations #{winner(board)}!"
   else
     puts "Cat's Game!"
   end
